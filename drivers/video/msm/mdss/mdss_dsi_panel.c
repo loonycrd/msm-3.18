319e9da44ac853aed6d68de91521f0ba123c56bc
Rohit Vashisht <rohitv88cf@gmail.com>
2018-06-09 00:01:22 +0530

    mido: fix a build error and warning
    These were fixed with tissot merge but because I skipped it...
    
     - error: Fix undefined reference to allow external access to function [mdss_dsi_panel.c]
     - warning: Be lazy and remove only the unused variable irrelevant for mido atm [msm8952.c]
    
    Signed-off-by: Rohit Vashisht <rohitv88cf@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -182,7 +182,7 @@ static void mdss_dsi_panel_apply_settings(struct mdss_dsi_ctrl_pdata *ctrl,
 	mdss_dsi_cmdlist_put(ctrl, &cmdreq);
 }
 
-static void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
+void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
 			struct dsi_panel_cmds *pcmds, u32 flags)
 {
 	struct dcs_cmd_req cmdreq;
