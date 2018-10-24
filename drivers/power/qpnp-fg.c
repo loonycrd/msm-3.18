628be6ee37a43589832f06099b24ce6993acbf3e
Raphiel Rollerscaperers <raphielscape@outlook.com>
2018-10-24 13:37:18 +0700

    power: qpnp-fg: saner waketime?
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>
    Signed-off-by: Rohit Vashisht <rohitv88cf@gmail.com>

--- drivers/power/qpnp-fg.c
+++ drivers/power/qpnp-fg.c
@@ -334,11 +334,7 @@ module_param_named(
 	battery_type, fg_batt_type, charp, S_IRUSR | S_IWUSR
 );
 
-#ifdef CONFIG_MACH_XIAOMI_MIDO
-static int fg_sram_update_period_ms = 3000;
-#else
 static int fg_sram_update_period_ms = 30000;
-#endif
 module_param_named(
 	sram_update_period_ms, fg_sram_update_period_ms, int, S_IRUSR | S_IWUSR
 );
